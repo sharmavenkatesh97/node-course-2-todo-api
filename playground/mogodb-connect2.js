dc010d438a27e4f5e0016768c632a1bd91e4c581
sharmavenkatesh97 <46029879+sharmavenkatesh97@users.noreply.github.com>
2019-02-28 12:24:31 +0530

    updation and deletion

--- playground/mogodb-connect2.js
+++ playground/mogodb-connect2.js
@@ -28,14 +28,45 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   //   console.log('Unable to connect to mongo');
   //
   // });
-  db.collection('Users').find({
-    name: "hello"
-  }).toArray().then((docs) => {
-    console.log('Todos');
-    console.log(JSON.stringify(docs, undefined, 2));
-  }, (err) => {
-    console.log('Unable to connect to mongo');
+  // db.collection('Users').find({
+  //   name: "hello"
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to connect to mongo');
+  //
+  // });
 
+  // db.collection('Users').deleteMany({
+  //   name: 'venky'
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5c765607e2d9862a58f4cd2a')
+  }, {
+    $set: {
+      name: 'venkyyyy'
+    },
+    $inc: {
+      age: 4
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
   });
+  // db.collections('ToDos').findOneAndDelete({})
+
+
+
+
+
+
+
+
+
   // client.close();
 });
